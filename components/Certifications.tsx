df2249d4a85399bcebde8c535a370532e4a528db
z0neSec <niklausdavid30@gmail.com>
2026-06-17 10:19:49 +0100

    fix: figma svg

--- components/Certifications.tsx
+++ components/Certifications.tsx
@@ -19,7 +19,7 @@ function GoogleG() {
 /* Cleaner Figma logo */
 function FigmaLogo() {
   return (
-    <svg viewBox="0 0 38 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 70 57" xmlns="http://www.w3.org/2000/svg">
       <path d="M19 28.5A9.5 9.5 0 0 1 28.5 19H38v19H28.5A9.5 9.5 0 0 1 19 28.5z" fill="#1ABCFE"/>
       <path d="M0 47.5A9.5 9.5 0 0 1 9.5 38H19v9.5A9.5 9.5 0 1 1 0 47.5z" fill="#0ACF83"/>
       <path d="M19 0h9.5A9.5 9.5 0 1 1 19 19V0z" fill="#FF7262"/>
